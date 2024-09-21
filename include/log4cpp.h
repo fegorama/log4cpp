1e73abe24ed5c146f555e18cc2824372af78e9f5
Fernando Gonzalez <fegor@fegorsoft.com>
2024-09-21 14:18:56 +0200

    First upload

---
+++ include/log4cpp.h
@@ -0,0 +1,62 @@
+// Copyright 2024 - 2025, Fernando González Ruano (Fegor)
+// SPDX-License-Identifier: mit
+
+#ifndef LOG4CPP_H
+#define LOG4CPP_H
+
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+enum class LogLevel : int8_t
+{
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARNING,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_CRITICAL
+};
+
+class Log4Cpp
+{
+public:
+    Log4Cpp();
+    ~Log4Cpp();
+    void log(const LogLevel level, const string &message);
+    void debug(const string &message);
+    void info(const string &message);
+    void warning(const string &message);
+    void error(const string &message);
+    void critical(const string &message);
+    void setEnableConsole(const bool enable);
+    void setEnableFile(const bool enable);
+    void setEnableSyslog(const bool enable);
+    void setEnableSystemd(const bool enable);
+
+    //void setLogFile(const string filename);
+    //void setLogLevel(const LogLevel level);
+    LogLevel getLogLevel() const;
+    bool isDebugEnabled();
+    bool isInfoEnabled();
+    bool isWarningEnabled();
+    bool isErrorEnabled();
+    bool isCriticalEnabled();
+
+    static LogLevel logLevel;
+    static string fileName;
+    static bool enableConsole;
+    static bool enableFile;
+    static bool enableSyslog;
+    static bool enableSystemd;
+
+private:
+    ofstream logFile;
+
+    void openLogFile();
+    const string levelToString(const LogLevel level) const;
+};
+
+#endif
---
+++ src/log4cpp.cpp
@@ -0,0 +1,198 @@
+// Copyright 2024 - 2025, Fernando González Ruano (Fegor)
+// SPDX-License-Identifier: mit
+
+// Based on https://www.geeksforgeeks.org/logging-system-in-cpp/
+
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "log4cpp.h"
+
+using namespace std;
+
+bool Log4Cpp::enableConsole = true;
+bool Log4Cpp::enableFile = true;
+bool Log4Cpp::enableSyslog = false;
+bool Log4Cpp::enableSystemd = false;
+
+Log4Cpp::Log4Cpp()
+{
+	this->openLogFile();
+}
+
+Log4Cpp::~Log4Cpp()
+{
+	logFile.close();
+}
+
+void Log4Cpp::debug(const string &message)
+{
+	this->log(LogLevel::LOG_LEVEL_DEBUG, message);
+}
+
+void Log4Cpp::info(const string &message)
+{
+	this->log(LogLevel::LOG_LEVEL_INFO, message);
+}
+
+void Log4Cpp::warning(const string &message)
+{
+	this->log(LogLevel::LOG_LEVEL_WARNING, message);
+}
+
+void Log4Cpp::error(const string &message)
+{
+	this->log(LogLevel::LOG_LEVEL_ERROR, message);
+}
+
+void Log4Cpp::critical(const string &message)
+{
+	this->log(LogLevel::LOG_LEVEL_CRITICAL, message);
+}
+
+void Log4Cpp::log(const LogLevel level, const string &message)
+{
+	// Get current timestamp
+	time_t now = time(0);
+	tm *timeinfo = localtime(&now);
+	char timestamp[20];
+	strftime(timestamp, sizeof(timestamp),
+			 "%Y-%m-%d %H:%M:%S", timeinfo);
+
+	ostringstream logEntry;
+	logEntry << "[" << timestamp << "] "
+			 << levelToString(level) << ": " << message
+			 << endl;
+
+	if (this->enableConsole)
+		cout << logEntry.str();
+
+	if (this->enableFile)
+		if (logFile.is_open())
+		{
+			logFile << logEntry.str();
+			logFile.flush();
+		}
+}
+
+void Log4Cpp::setEnableConsole(const bool enable)
+{
+	Log4Cpp::enableConsole = enable;
+}
+
+void Log4Cpp::setEnableFile(const bool enable)
+{
+	Log4Cpp::enableFile = enable;
+}
+
+void Log4Cpp::setEnableSyslog(const bool enable)
+{
+	Log4Cpp::enableSyslog = enable;
+}
+
+void Log4Cpp::setEnableSystemd(const bool enable)
+{
+	Log4Cpp::enableSystemd = enable;
+}
+
+/*
+void Log4Cpp::setLogFile(const string filename)
+{
+	openLogFile(filename);
+}
+*/
+/*
+void Log4Cpp::setLogLevel(const LogLevel level)
+{
+	logLevel = level;
+}
+*/
+
+LogLevel Log4Cpp::getLogLevel() const
+{
+	return Log4Cpp::logLevel;
+}
+
+void Log4Cpp::openLogFile()
+{
+	logFile.open(this->fileName, ios::app);
+
+	if (!logFile.is_open())
+	{
+		cerr << "Error opening log file." << endl;
+		exit(1);
+	}
+}
+
+bool Log4Cpp::isDebugEnabled()
+{
+	return this->logLevel == LogLevel::LOG_LEVEL_DEBUG;
+}
+
+bool Log4Cpp::isInfoEnabled()
+{
+	return this->logLevel == LogLevel::LOG_LEVEL_INFO;
+}
+
+bool Log4Cpp::isWarningEnabled()
+{
+	return this->logLevel == LogLevel::LOG_LEVEL_WARNING;
+}
+
+bool Log4Cpp::isErrorEnabled()
+{
+	return this->logLevel == LogLevel::LOG_LEVEL_ERROR;
+}
+
+bool Log4Cpp::isCriticalEnabled()
+{
+	return this->logLevel == LogLevel::LOG_LEVEL_CRITICAL;
+}
+
+const string Log4Cpp::levelToString(const LogLevel level) const
+{
+	switch (level)
+	{
+	case LogLevel::LOG_LEVEL_DEBUG:
+		return "DEBUG";
+	case LogLevel::LOG_LEVEL_INFO:
+		return "INFO";
+	case LogLevel::LOG_LEVEL_WARNING:
+		return "WARNING";
+	case LogLevel::LOG_LEVEL_ERROR:
+		return "ERROR";
+	case LogLevel::LOG_LEVEL_CRITICAL:
+		return "CRITICAL";
+	default:
+		return "UNKNOWN";
+	}
+}
+
+int main()
+{
+	Log4Cpp logger;
+
+	Log4Cpp::fileName = "prueba.log";
+	Log4Cpp::logLevel = LogLevel::LOG_LEVEL_ERROR;
+
+	if (logger.isInfoEnabled())
+	{
+		logger.info("Info.");
+	}
+	else if (logger.isDebugEnabled())
+	{
+		logger.debug("Debug.");
+	}
+	else if (logger.isErrorEnabled())
+	{
+		logger.error("Error.");
+	}
+	else
+	{
+		logger.critical("Critical.");
+	}
+
+	return 0;
+}
